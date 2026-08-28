353755f05baf55c5042f68cbbbbfcfb2f57e4760
sukruthsudheesh <sukruthsudheesh@gmail.com>
2026-08-28 14:27:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,18 +14,18 @@ let currentPlayer = 1;
 let player1Score = 0;
 let player2Score = 0;
 const symbols = [
-    "images/1.png",
-    "images/2.png",
-    "images/3.png",
-    "images/4.png",
-    "images/5.png",
-    "images/6.png",
-    "images/7.png",
-    "images/8.png",
-    "images/9.png",
-    "images/10.png",
-    "images/11.png",
-    "images/12.png"
+    "1.png",
+    "2.png",
+    "3.png",
+    "4.png",
+    "5.png",
+    "6.png",
+    "7.png",
+    "8.png",
+    "9.png",
+    "10.png",
+    "11.png",
+    "12.png"
 ];
 
 let flippedCards = [];
